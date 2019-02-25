569cf67927f8d655d8905e713c13ea94ab9be586
thomasfay <thomas.fay@esss.se>
2019-02-25 14:28:37 +0100

    Added document fetcher script.

---
+++ getDocs.bash
@@ -0,0 +1,10 @@
+if [ ! - d docs ]; then
+  mkdir docs
+fi
+if [ ! -d docs ]; then
+  echo "Failed to create directory in current folder. Check write permissions for user."
+fi
+
+cd docs
+
+eval "../ConOps-cntrl"
